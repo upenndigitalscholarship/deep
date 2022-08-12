3fcaa71799cf7fcf92e07ecef6e6b46bf0cce2f7
apjanco <apjanco@gmail.com>
2022-08-12 17:30:01 -0400

    end of spring, moving to gather collection, variant and other data from site, not db

--- main/management/commands/validate_itemjson.py
+++ main/management/commands/validate_itemjson.py
@@ -12,9 +12,15 @@ def validate_item(item:dict):
     driver.get(f"http://deep.sas.upenn.edu/{item['deep_id']}")
     deep_id = driver.find_element(By.XPATH, "/html/body/div/div[2]/table/tbody/tr[2]/td/div/div[1]/div[1]/div[1]").text.replace('DEEP #: ','')
     assert deep_id == item['deep_id'], 'error: '+ item['deep_id']
+    greg = driver.find_element(By.XPATH,"/html/body/div/div[2]/table/tbody/tr[2]/td/div/div[1]/div[1]/div[2]").text.replace('GREG #: ','')
+    assert greg == item['greg_full'], 'error: '+ item['greg_full'] + '---' + greg
+
+    stc_wing = driver.find_element(By.XPATH,"/html/body/div/div[2]/table/tbody/tr[2]/td/div/div[1]/div[1]/div[3]").text.replace('STC/WING #: ','')
+    assert stc_wing == item['stc'], 'error: '+ item['stc'] + '---' + stc_wing
 
     record_type = driver.find_element(By.XPATH, '/html/body/div/div[2]/table/tbody/tr[2]/td/div/div[1]/div[2]/div[1]').text.replace('RECORD TYPE: ','')
     assert item['record_type'] == record_type, 'error: '+ item['record_type'] + ':' + record_type
+    #driver.close()
     return item, False
             #number = record.find_element(By.CLASS_NAME, "numcol").text
 
