0e772ed9e1caa30d162c510311e8d4c7b89df19e
apjanco <apjanco@gmail.com>
2022-09-09 18:15:48 +0000

    fixed expandAll()

--- deep/settings.py
+++ deep/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['159.223.189.230','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['deep.pennds.org']
 
 
 # Application definition
