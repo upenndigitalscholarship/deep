a7b0465c60e59df5ca92def306a01044f31c5254
apjanco <apjanco@gmail.com>
2023-01-19 11:51:21 -0500

    adding formatted variant description from db

---
+++ main/management/commands/add_variant_description.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from pathlib import Path
+from tqdm import tqdm
+from main.models import *
+import numpy as np
+
+class Command(BaseCommand):
+    help = 'Load Has variant_description'
+    
+    def handle(self, *args, **options):
+        txt_file = Path('backup/variant_description.tsv').read_text()
+        for line in txt_file.split('\n'):
+            deep_id, variant_description = line.split('\t')
+            item = Item.objects.get(deep_id=deep_id)
+            item.variant_description = variant_description
+            item.save() 
+            
+            
+        
