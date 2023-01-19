43aa2181948c330b4be88a72af54f08dd7e0e66b
apjanco <apjanco@gmail.com>
2023-01-19 15:46:01 -0500

    fix indents

--- main/management/commands/add_date_first_performance.py
+++ main/management/commands/add_date_first_performance.py
@@ -9,10 +9,10 @@ class Command(BaseCommand):
     help = 'Load title.date_first_performance from web data'
     
     def handle(self, *args, **options):
-web_data = srsly.read_jsonl('web_item_data.jsonl')
-for item in web_data:
-    deep = Item.objects.get(deep_id=item['deep_id'])
-    title = deep.edition.title
-    title.date_first_performance = item['date_first_performance']
+        web_data = srsly.read_jsonl('web_item_data.jsonl')
+        for item in web_data:
+            deep = Item.objects.get(deep_id=item['deep_id'])
+            title = deep.edition.title
+            title.date_first_performance = item['date_first_performance']
             title.save()
             #print(item['deep_id'], item['date_first_performance'])
