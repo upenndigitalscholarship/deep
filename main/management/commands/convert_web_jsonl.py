22805ea6730ba272faef803c408b7400a27d4482
apjanco <apjanco@gmail.com>
2023-02-01 09:38:49 -0500

    import using work ids

--- main/management/commands/convert_web_jsonl.py
+++ main/management/commands/convert_web_jsonl.py
@@ -114,7 +114,7 @@ def remove_variant_link_text(item:dict):
         return item["variants"]      
 
 class Command(BaseCommand):
-    help = 'Load jsonl file to Django models'
+    help = 'Load jsonl file to Django models.'
     
 
 
@@ -179,8 +179,8 @@ class Command(BaseCommand):
                 if record_type == 'Collection':
                     edition = Edition.objects.get(title=title,collection=collection)
                 else:
-                    if greg_middle != 'n/a':
-                        edition = Edition.objects.get(title=title,greg_middle=greg_middle)
+                    edition = Edition.objects.get(title=title,greg_middle=greg_middle)
+
             except Edition.DoesNotExist:
                 # use deep_id to fetch item data
                 db_item_data = lookup(item_data, deep_id)
@@ -210,7 +210,7 @@ class Command(BaseCommand):
             work_id_lookup[deep_id] = work_id
 
         web_data = srsly.read_jsonl('web_item_data.jsonl')
-        for item in tqdm(web_data):
+        for item in tqdm(web_data, total=1908):
             #There are three empty records to ignore
             if item["deep_id"] in ['47','48','1014']:
                 continue 
@@ -221,115 +221,112 @@ class Command(BaseCommand):
             
             
             title = Title.objects.get(work_id=work_id_lookup[item['deep_id']]) 
+            editions_to_fix = []
             if db_item_data['record_type'] == 'Collection':
-                    edition = Edition.objects.get(title=title,collection=db_item_data['collection'])
+                try:
+                    edition = Edition.objects.get(title=title,collection=db_item_data['collection_full'])
+                except Edition.DoesNotExist:
+                    editions_to_fix.append(db_item_data['collection_full'])
             else:
                 if greg_middle != 'n/a':
                     edition = Edition.objects.get(title=title,greg_middle=db_item_data['greg_middle'])
-            print('yay!')
-        #     # Create Edition objects
-        #     if title:
-        #         if p_edition and p_edition != 0:
-        #             edition = Edition.objects.get(title = title, play_edition=db_item_data['play_edition'])    
-        #         elif b_edition and b_edition != 0:
-        #             edition = Edition.objects.get(title = title, book_edition=db_item_data['book_edition'])  
-                
-                
-        #         if edition:
-        #             authors = get_authors(db_item_data)
-        #             edition.authors.add(*authors)
-        #             #playtype = get_playtype(item)
-        #             #edition.play_type.add(*playtype)
-        #             edition.save()
-                
-        #             # Create Item object
-        #             django_item, created = Item.objects.get_or_create(
-        #                 edition = edition,
-        #                 year = item['year'],
-        #                 year_int = db_item_data['year'],
-        #                 date_first_publication = item['date_first_publication_display'],
-        #                 record_type=django_is_worst(item['record_type']),
-        #                 collection = db_item_data['collection_full'],
-        #                 deep_id = item['deep_id'],
-        #                 greg_full = item['greg_full'], #The app transforms these, need to use web data
-        #                 stc = item['stc'],
-        #                 format = item['format'],
-        #                 leaves = item['leaves'],
-        #                 composition_date = db_item_data['composition_date_display'],
-        #                 company_attribution = item['company_attribution'],
-        #                 title_page_title = item["title_page_title"],
-        #                 title_page_author = item["title_page_author"],
-        #                 title_page_performance = item["title_page_performance"],
-        #                 title_page_latin_motto = item["title_page_latin_motto"],
-        #                 title_page_imprint = item["title_page_imprint"],
-        #                 title_page_illustration = item["title_page_illustration"],
-        #                 paratext_explicit = item["paratext_explicit"],
-        #                 stationer_colophon = item["stationer_colophon"],
-        #                 title_page_modern_spelling = db_item_data['transcript_modern_spelling'],
-        #                 paratext_errata = item["paratext_errata"],
-        #                 paratext_commendatory_verses = item["paratext_commendatory_verses"],
-        #                 paratext_to_the_reader = item["paratext_to_the_reader"],
-        #                 paratext_dedication = item["paratext_dedication"], 
-        #                 paratext_argument = item["paratext_argument"],
-        #                 paratext_actor_list = item["paratext_actor_list"],
-        #                 paratext_charachter_list = item["paratext_charachter_list"],
-        #                 paratext_other_paratexts = item["paratext_other_paratexts"],
-        #                 stationer_entries_in_register = item["stationer_entries_in_register"],
-        #                 stationer_additional_notes = item["stationer_additional_notes"],
-        #                 theater_type = db_item_data["theater_type"],
-        #                 theater = db_item_data["theater"],
-        #                 collection_full= db_item_data['collection_full'],
-        #                 collection_middle = db_item_data['collection_middle'],
-        #                 collection_brief = db_item_data['collection_brief'],
-        #                 variant_edition_id= db_item_data['variant_edition_id'],
-        #                 variant_newish_primary_deep_id = db_item_data['variant_newish_primary_deep_id'],
-        #                 author_status= db_item_data['author_status'],
-        #                 srstationer = db_item_data['srstationer'],
-        #                 publisher = db_item_data['publisher'],
-        #                 printer = db_item_data['printer']
-        #             )
-        #             stationer_printer = get_stationer_printer(db_item_data)
-        #             django_item.stationer_printer.add(*stationer_printer)
-                    
-        #             stationer_publisher = get_stationer_publisher(db_item_data)
-        #             django_item.stationer_publisher.add(*stationer_publisher)
 
-        #             stationer_bookseller = get_stationer_bookseller(db_item_data)
-        #             django_item.stationer_bookseller.add(*stationer_bookseller)
-                    
-        #             django_item.company, _ = Company.objects.get_or_create(name=django_item.company_attribution)
-        #             django_item.save()
+            if title and edition:
+                authors = get_authors(db_item_data)
+                edition.authors.add(*authors)
+                #playtype = get_playtype(item)
+                #edition.play_type.add(*playtype)
+                edition.save()
+            
+                # Create Item object
+                django_item, created = Item.objects.get_or_create(
+                    edition = edition,
+                    year = item['year'],
+                    year_int = db_item_data['year'],
+                    date_first_publication = item['date_first_publication_display'],
+                    record_type=django_is_worst(item['record_type']),
+                    collection = db_item_data['collection_full'],
+                    deep_id = item['deep_id'],
+                    greg_full = item['greg_full'], #The app transforms these, need to use web data
+                    stc = item['stc'],
+                    format = item['format'],
+                    leaves = item['leaves'],
+                    composition_date = db_item_data['composition_date_display'],
+                    company_attribution = item['company_attribution'],
+                    title_page_title = item["title_page_title"],
+                    title_page_author = item["title_page_author"],
+                    title_page_performance = item["title_page_performance"],
+                    title_page_latin_motto = item["title_page_latin_motto"],
+                    title_page_imprint = item["title_page_imprint"],
+                    title_page_illustration = item["title_page_illustration"],
+                    paratext_explicit = item["paratext_explicit"],
+                    stationer_colophon = item["stationer_colophon"],
+                    title_page_modern_spelling = db_item_data['transcript_modern_spelling'],
+                    paratext_errata = item["paratext_errata"],
+                    paratext_commendatory_verses = item["paratext_commendatory_verses"],
+                    paratext_to_the_reader = item["paratext_to_the_reader"],
+                    paratext_dedication = item["paratext_dedication"], 
+                    paratext_argument = item["paratext_argument"],
+                    paratext_actor_list = item["paratext_actor_list"],
+                    paratext_charachter_list = item["paratext_charachter_list"],
+                    paratext_other_paratexts = item["paratext_other_paratexts"],
+                    stationer_entries_in_register = item["stationer_entries_in_register"],
+                    stationer_additional_notes = item["stationer_additional_notes"],
+                    theater_type = db_item_data["theater_type"],
+                    theater = db_item_data["theater"],
+                    collection_full= db_item_data['collection_full'],
+                    collection_middle = db_item_data['collection_middle'],
+                    collection_brief = db_item_data['collection_brief'],
+                    variant_edition_id= db_item_data['variant_edition_id'],
+                    variant_newish_primary_deep_id = db_item_data['variant_newish_primary_deep_id'],
+                    author_status= db_item_data['author_status'],
+                    srstationer = db_item_data['srstationer'],
+                    publisher = db_item_data['publisher'],
+                    printer = db_item_data['printer']
+                )
+                stationer_printer = get_stationer_printer(db_item_data)
+                django_item.stationer_printer.add(*stationer_printer)
+                
+                stationer_publisher = get_stationer_publisher(db_item_data)
+                django_item.stationer_publisher.add(*stationer_publisher)
 
+                stationer_bookseller = get_stationer_bookseller(db_item_data)
+                django_item.stationer_bookseller.add(*stationer_bookseller)
+                
+                django_item.company, _ = Company.objects.get_or_create(name=django_item.company_attribution)
+                django_item.save()
+            else:
+                print(f'error, no title/edition {item["deep_id"]}')
         
-        # self.stdout.write(self.style.SUCCESS('adding variants and collection links'))
-        # web_data = srsly.read_jsonl('web_item_data.jsonl')
-        # for item in tqdm(web_data):
-        #     if item["deep_id"] in ["1014","47","48"]:
-        #         continue
-        #     else:
-        #         django_item = Item.objects.get(deep_id=item["deep_id"])
-        #         django_item.variants = remove_variant_link_text(item)
-        #         variant_links = get_variant_links(item)
-        #         django_item.variant_links.add(*variant_links)
+        self.stdout.write(self.style.SUCCESS('adding variants and collection links'))
+        web_data = srsly.read_jsonl('web_item_data.jsonl')
+        for item in tqdm(web_data, total=1908):
+            if item["deep_id"] in ["1014","47","48"]:
+                continue
+            else:
+                django_item = Item.objects.get(deep_id=item["deep_id"])
+                django_item.variants = remove_variant_link_text(item)
+                variant_links = get_variant_links(item)
+                django_item.variant_links.add(*variant_links)
 
-        #         collection_links = get_collection_links(item)
-        #         django_item.collection_contains.add(*collection_links)
-        #         if item["in_collection"]:
-        #             django_item.in_collection = Link.objects.get(deep_id=item["in_collection_link_href"])
+                collection_links = get_collection_links(item)
+                django_item.collection_contains.add(*collection_links)
+                if item["in_collection"]:
+                    django_item.in_collection = Link.objects.get(deep_id=item["in_collection_link_href"])
                 
-        #         if item["independent_playbook"]:
-        #             django_item.independent_playbook = item["independent_playbook"]
-        #             try:
-        #                 django_item.independent_playbook_link = Link.objects.get(deep_id=item["independent_playbook_link_href"])
-        #             except Link.DoesNotExist:
-        #                 print('broken link, ask how to handle', item["independent_playbook_link_href"])
-        #         if item["also_in_collection"]:
-        #             django_item.also_in_collection = item["also_in_collection"]
-        #             django_item.also_in_collection_link = Link.objects.get(deep_id=item["also_in_collection_link_href"])
-        #         django_item.stationer_bookseller_display = item['stationer_bookseller']
-        #         django_item.stationer_publisher_display = item['stationer_publisher']
-        #         django_item.stationer_printer_display = item['stationer_printer']
+                if item["independent_playbook"]:
+                    django_item.independent_playbook = item["independent_playbook"]
+                    try:
+                        django_item.independent_playbook_link = Link.objects.get(deep_id=item["independent_playbook_link_href"])
+                    except Link.DoesNotExist:
+                        print('broken link, ask how to handle', item["independent_playbook_link_href"])
+                if item["also_in_collection"]:
+                    django_item.also_in_collection = item["also_in_collection"]
+                    django_item.also_in_collection_link = Link.objects.get(deep_id=item["also_in_collection_link_href"])
+                django_item.stationer_bookseller_display = item['stationer_bookseller']
+                django_item.stationer_publisher_display = item['stationer_publisher']
+                django_item.stationer_printer_display = item['stationer_printer']
                 
-        #         django_item.save()
-       
+                django_item.save()
+        print("fix these editions", editions_to_fix)
 
