155df51e1eb59da86a0b41681f2ca798a50c0968
apjanco <apjanco@gmail.com>
2023-08-21 17:33:42 -0400

    || not allowed in filer?

--- main/management/commands/search_index.py
+++ main/management/commands/search_index.py
@@ -39,6 +39,8 @@ def item_to_dict(item:Item):
         item_dict["title_page_modern_spelling"] = ''
     if not item_dict.get('title_page_latin_motto',None): 
         item_dict["title_page_latin_motto"] = ''
+    if not item_dict.get('title_page_illustration',None): 
+        item_dict["title_page_illustration"] = ""
     if not item_dict.get('title_page_author',None): 
         item_dict["title_page_author"] = 'None'
     if not item_dict.get("title_page_performance",None):
