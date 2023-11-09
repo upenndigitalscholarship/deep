84b73190e38f046d507d33c1ea47a5d37cc82a83
apjanco <apjanco@gmail.com>
2023-11-09 10:47:26 -0500

    #180

--- main/assets/js/deep.js
+++ main/assets/js/deep.js
@@ -2047,7 +2047,7 @@ function expand(e, deep_id) {
                 ${!data.stationer_bookseller_filter ? '' : '<span class="expand">Bookseller: </span><span id="stationer_bookseller">' + data.stationer_bookseller_display + '</span><br>'}
                 ${data.stationer_imprint_location === "None" ? '' : '<span class="expand">Imprint Location: </span><span id="stationer_imprint_location">' + data.stationer_imprint_location + '</span><br>'}
                 ${"<span class='expand'>Entries in Stationers' Registers: </span><span id='stationer_entries_in_register'>" + data.stationer_entries_in_register + '</span><br>'}
-                ${data.stationer_additional_notes === '' || data.stationer_additional_notes === null ? '' : '<span class="expand"></span>'} 
+                ${!data.stationer_additional_notes ? '' : '<span class="expand"></span>'} 
                 ${!data.stationer_additional_notes ? '' : '<span class="expand">Additional Notes: </span><span id="stationer_additional_notes">' + data.stationer_additional_notes + '</span><br>'}
               </p>
             </div>
