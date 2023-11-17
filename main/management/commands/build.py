4c2994fe86cc8b55ad55e63f473dcb3091899a83
apjanco <apjanco@gmail.com>
2023-11-17 13:21:47 -0500

    #136

--- main/management/commands/build.py
+++ main/management/commands/build.py
@@ -286,12 +286,13 @@ class Command(BaseCommand):
                 'value': i,
                 'label': form.strip()
             })
-        formats_json.insert(0, {"value":0,"label":"Broadsheet" })
-        formats_json.insert(0, {"value":1,"label":"Folio" })
-        formats_json.insert(0, {"value":2,"label":"Quarto" })
-        formats_json.insert(0, {"value":3,"label":"Octavo" })
-        formats_json.insert(0, {"value":4,"label":"Duodecimo" })
         formats_json.insert(0, {"value":5,"label":"Sixteenmo" })
+        formats_json.insert(0, {"value":4,"label":"Duodecimo" })
+        formats_json.insert(0, {"value":4,"label":"Duodecimo" })
+        formats_json.insert(0, {"value":3,"label":"Octavo" })
+        formats_json.insert(0, {"value":2,"label":"Quarto" })
+        formats_json.insert(0, {"value":1,"label":"Folio" })
+        formats_json.insert(0, {"value":0,"label":"Broadsheet" })
         srsly.write_json(static_dir / 'data/formats.json', formats_json)
 
         #Printer
