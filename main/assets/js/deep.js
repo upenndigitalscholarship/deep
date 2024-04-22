caff1ebac045d1962fdbd3b714b41f06947e8127
Andrew Janco <apjanco@gmail.com>
2024-04-22 14:22:57 -0400

    Update deep.js
    change from replace first occurrence of punctuation, to all

--- main/assets/js/deep.js
+++ main/assets/js/deep.js
@@ -472,7 +472,7 @@ const update_searchSelect = (searchSelect, or = false) => {
 }
 
 const noPunct = string => {
-  return string.replace(',', '').replace('.', '').replace(':', '').replace(';', '').replace('?', '').replace('-', '').replace('_', '')
+  return string.replace(/\,/g,"").replace(/\./g,"").replace(/\:/g,"").replace(/\;/g,"").replace(/\?/g,"").replace(/\-/g,"").replace(/\_/g,"")
 }
 
 const getQueries = () => {
