7820b846ac3ee01d8e6b3f2b3f0b84dc250182ea
mhunter52 <mhunter52@gmail.com>
2026-05-28 10:51:57 -0400

    add current URL to allowed host list

--- deep/settings.py
+++ deep/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['deep.pennds.org','localhost']
+ALLOWED_HOSTS = ['deep.pennds.org','localhost','deepplaybooks.org']
 
 
 # Application definition
